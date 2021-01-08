b7f06dc42c36461bfcd740351a90a902e1271710
Sergey-Proskurnin <proskurnin@ua.fm>
2021-01-08 02:29:05 +0200

    Сделал и добавил задания 1-5 дополнительного ДЗ-2

---
+++ js/task-3r.js
@@ -0,0 +1,26 @@
+// Задание 3
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку(в строке будут только слова и пробелы)
+// и возвращает самое длинное слово в этой строке.
+
+const findLongestWord = function(string) {
+    const words = string.split(' ');
+    let bigWord = words[0];
+    
+    for (let word of words) {
+        
+        if (bigWord.length < word.length) {
+            bigWord = word;
+        }
+    }
+    return bigWord
+    // твой код
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
